90da4bd51649500158faf6eafd00de72f1a4b58d
kbmodhia <kaustubhbmodhia@gmail.com>
2021-06-01 14:59:49 +0530

    add new var files

--- teraform_vars_explain/createinstance.tf
+++ teraform_vars_explain/createinstance.tf
@@ -1,10 +1,4 @@
 resource "aws_instance" "MysFirstInstance" {
-  count = 3 //it will createe 3 instance
   ami = "ami-0d563aeddd4be7fff" // get it from aws cloude ami info
   instance_type = "t2.micro" // get it from aws 
-
-  tags = {
-      //name = "threedemoinstance" // git same name to all instance
-      name = "demoinstance-${count.index}" // it will give dynamic anme to all instance
-  }
 }
