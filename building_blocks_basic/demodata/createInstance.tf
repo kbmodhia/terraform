02b360aeef5244f0d7aa92b6c7bd164b85ade34d
kbmodhia <kaustubhbmodhia@gmail.com>
2021-06-02 10:18:25 +0530

    fix code

--- building_blocks_basic/demodata/createInstance.tf
+++ building_blocks_basic/demodata/createInstance.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "MyFirstInstnace" {
   //ami           = lookup(var.AMIS, var.AWS_REGION)
   ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zone.available.name[1]
+  availability_zone = data.aws_availability_zone.available.names[1]
 
   tags = {
     Name = "custom_instance"
